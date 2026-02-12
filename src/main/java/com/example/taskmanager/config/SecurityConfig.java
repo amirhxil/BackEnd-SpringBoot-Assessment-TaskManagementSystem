d87674a39781d31e0338d0f9cc3179a110204071
Muhammad Amir Hail <amir.ryugasaki@gmail.com>
2026-02-13 06:09:07 +0800

    solve some warning

--- src/main/java/com/example/taskmanager/config/SecurityConfig.java
+++ src/main/java/com/example/taskmanager/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,8 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 import javax.crypto.spec.SecretKeySpec;
+import org.springframework.security.config.Customizer;
+
 
 @Configuration
 public class SecurityConfig {
@@ -18,12 +21,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll() // login & register open
                         .anyRequest().authenticated()               // all other APIs require auth
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt());  // enable JWT auth
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
 
         return http.build();
     }
